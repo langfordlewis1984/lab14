cfe57cd898390f61063082efe55409917e5c498b
Nick Lewis <nick@TechEd-0006.techeducators.co.uk>
2023-06-29 15:28:50 +0100

    oddDuck Exercise

--- js/app.js
+++ js/app.js
@@ -1,37 +1,62 @@
-'use strict';
+"use strict";
 
 function AppState() {
   this.allProducts = [];
 }
 
 AppState.prototype.instantiateProducts = function () {
-
-  const productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
+  const productNames = [
+    "bag",
+    "banana",
+    "bathroom",
+    "boots",
+    "breakfast",
+    "bubblegum",
+    "chair",
+    "cthulhu",
+    "dog-duck",
+    "dragon",
+    "pen",
+    "pet-sweep",
+    "scissors",
+    "shark",
+    "sweep",
+    "tauntaun",
+    "unicorn",
+    "water-can",
+    "wine-glass",
+  ];
 
   for (let i = 0; i < productNames.length; i++) {
-    if (productNames[i] === 'sweep') {
-      this.allProducts.push(new Product(productNames[i], 'png'))
+    if (productNames[i] === "sweep") {
+      this.allProducts.push(new Product(productNames[i], "png"));
     } else {
-      this.allProducts.push(new Product(productNames[i]))
+      this.allProducts.push(new Product(productNames[i]));
     }
   }
-
-}
+};
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
-}
+
+  const productsToStore = JSON.stringify(this.allProducts);
+
+  localStorage.setItem("products", productsToStore);
+};
 
 AppState.prototype.loadItems = function () {
-
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
 
-  this.instantiateProducts();
+  const storedProducts = localStorage.getItem("products");
 
-}
+  if (storedProducts) {
+    this.allProducts = JSON.parse(storedProducts);
+  } else {
+    this.instantiateProducts();
+  }
+};
 
-
-function Product(name, fileExtension = 'jpg') {
+function Product(name, fileExtension = "jpg") {
   this.name = name;
   this.source = `assets/${name}.${fileExtension}`;
   this.timesClicked = 0;
--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,62 @@
-'use strict';
+"use strict";
 
-let canvasElem = document.getElementById('chart')
+let canvasElem = document.getElementById("chart");
+
+Chart.defaults.font.size = "8px";
+Chart.defaults.color = "#0c5b2d";
+
+function renderChart() {
+  // * - Instantiate a new AppState
+
+  let appState = new AppState();
+
+  // * - Use a method on that AppState to load vote data from localStorage.
+
+  appState.loadItems();
+
+  // * - Create a data object for chart.js using your AppState's allProducts array.
+
+  // Create empty arrays for names, views and clicks
+
+  let productNames = [];
+  let productViews = [];
+  let productClicks = [];
+
+  // fill labels and data arrays
+
+  for (let i = 0; i < appState.allProducts.length; i++) {
+    productNames.push(appState.allProducts[i].name);
+    productViews.push(appState.allProducts[i].timesClicked);
+    productClicks.push(appState.allProducts[i].timesShown);
+  }
+
+  const data = {
+    labels: productNames,
+    datasets: [
+      {
+        label: "Clicks",
+        data: productClicks,
+        backgroundColor: ["#fec601"],
+        borderColor: ["#0c5b2d"],
+        borderWidth: 1,
+      },
+      {
+        label: "Views",
+        data: productViews,
+        backgroundColor: ["#0c5b2d"],
+        borderColor: ["#fec601"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+  };
+
+  const newChart = new Chart(canvasElem, config);
+}
 
 /* TODO:
  * - Instantiate a new AppState
@@ -10,7 +66,5 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
-function renderChart() {
-}
 
 renderChart();
